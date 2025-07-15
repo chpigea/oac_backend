1be15d891ea32c7b567f7975a3ad1079473f9652
Christian R. Picone <ch.picone@gmail.com>
2025-07-15 20:40:46 +0200

    update

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,41 +2,72 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../models/users');
 
-/**
-curl -X POST \
--H "Content-Type: application/json" \
--d '{"type": "login","user":"igea","password":"@igea#"}' \
-http://localhost:4000/backend/auth/authenticate
- */
-router.post('/authenticate', (req, res) => {  
-  console.log("here authenticate")
-  let body = req.body 
-  //let type = body.type
-  let user = body.user
-  let password = body.password
-  Users.fromAuthentication(user, password)
-  .then(response =>{
-    res.json({ 
-        success: true, 
-        data: response, 
-        message: null
-    });
-  }).catch(err => { 
-    res.json({ 
-        success: false, 
-        data: null, 
-        message: `${ err }`
-    });
-  })
-});
+module.exports = function(jwtLib){
+  /**
+  curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"type": "login","user":"igea","password":"@igea#"}' \
+  http://localhost:4000/backend/auth/authenticate
+  */
 
-router.get('/echo', (req, res) => {  
-    console.log("here ECHO")
-    res.json({ 
-        success: true, 
-        data: "echo", 
-        message: null
-    });
-});
+  const manageUser = function(res, user){
+    let token = jwtLib.createToken(user)
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'lax'
+      })
+      res.json({ 
+          success: true, 
+          data: user, 
+          message: null
+      });
+  }
 
-module.exports = router
+  router.post('/authenticate', (req, res) => {  
+    console.log("here authenticate")
+    let body = req.body 
+    let type = body.type
+    
+    if(type.toLowerCase()=="login"){
+      let user = body.user
+      let password = body.password
+      Users.fromAuthentication(user, password)
+      .then(response => {
+        response["is_anonymous"] = false
+        manageUser(res, response)
+      }).catch(err => { 
+        res.json({ 
+            success: false, 
+            data: null, 
+            message: `${ err }`
+        });
+      })
+    }else{
+      manageUser(res, {
+        id: -1,
+        name: 'anonymous',
+        surname: 'anonymous',
+        username: 'anonymous',
+        email: 'anonymous@igea-soluzioni.it',
+        mobile: null,
+        is_admin: false,
+        is_active: true,
+        is_anonymous: true
+      })
+    }
+  });
+
+  router.get('/echo', (req, res) => {  
+      console.log("here ECHO")
+      res.json({ 
+          success: true, 
+          data: "echo", 
+          message: null
+      });
+  });
+
+  return router
+
+}
+
